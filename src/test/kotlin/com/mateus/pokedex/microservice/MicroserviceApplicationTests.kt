7885e61cfc70124662e138ef29243be42018c52c
mateusfacundes <mateusmarchao@gmail.com>
2025-02-07 18:57:10 -0300

    initial commit

---
+++ src/test/kotlin/com/mateus/pokedex/microservice/MicroserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mateus.pokedex.microservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
