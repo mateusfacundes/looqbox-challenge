0a44ac14a4d33544231fa20b1e7cb75722a15753
mateusfacundes <mateusmarchao@gmail.com>
2025-02-08 17:29:16 -0300

    feat: implementation of merge sort on the pokemon list

--- src/main/kotlin/com/mateus/pokedex/microservice/model/Pokemons.kt
+++ src/main/kotlin/com/mateus/pokedex/microservice/model/Pokemons.kt
@@ -2,5 +2,4 @@ package com.mateus.pokedex.microservice.model
 
 class Pokemons {
     var name: String? = ""
-    var url: String? = ""
 }
--- src/main/kotlin/com/mateus/pokedex/microservice/service/PokemonService.kt
+++ src/main/kotlin/com/mateus/pokedex/microservice/service/PokemonService.kt
@@ -27,14 +27,65 @@ class PokemonService {
         // TODO: Make cache logic
         if (!::pokemonsList.isInitialized || pokemonsList.isEmpty()) {
             val url = pokemonQuery.createQuery().withLimit(1304).getUrl()
-            println(url)
             this.getPokemons(url)
         }
 
         //TODO: Filter pokemon list by name an sort it
-        println(name)
-        println(sort)
+        if (name != null) {
+
+        }
+
+        this.pokemonsList = this.mergeSortPokemonList(this.pokemonsList)
+        this.pokemonsList = this.filterPokemonList(this.pokemonsList)
+
+
         return this.pokemonsList
+    }
+
+    // Time Complexity: always O(n log n)
+    fun mergeSortPokemonList(pokeList: List<Pokemons>): List<Pokemons> {
+        if (pokeList.size <= 1)
+            return pokeList
+
+        val half: Int = pokeList.size / 2
+
+        var leftHalf = this.mergeSortPokemonList(pokeList.subList(0, half))
+        var rightHalf = this.mergeSortPokemonList(pokeList.subList(half, pokeList.size))
+
+        return this.merge(leftHalf, rightHalf)
+    }
+
+    fun merge(leftHalf: List<Pokemons>, rightHalf: List<Pokemons>): List<Pokemons> {
+        val results: MutableList<Pokemons> = emptyList<Pokemons>().toMutableList()
+        var i: Int = 0
+        var j: Int = 0
+
+        while (i < leftHalf.size && j < rightHalf.size) {
+            if (leftHalf[i].name!! <= rightHalf[j].name!!) {
+                results.add(leftHalf[i])
+                i++
+            } else {
+                results.add(rightHalf[j])
+                j++
+            }
+        }
+
+        while (i < leftHalf.size) {
+            results.add(leftHalf[i])
+            i++
+        }
+
+        while (j < rightHalf.size) {
+            results.add(rightHalf[j])
+            j++
+        }
+
+        return results
+    }
+
+    fun filterPokemonList(pokeList: List<Pokemons>): List<Pokemons> {
+
+        return pokeList
     }
 
 }
