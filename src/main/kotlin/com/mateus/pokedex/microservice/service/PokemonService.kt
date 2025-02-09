ea9916fd127dd2b4aa29a160a77535fe30e7e676
mateusfacundes <mateusmarchao@gmail.com>
2025-02-09 13:32:54 -0300

    feat: filterPokemonList func and sort typs added

--- src/main/kotlin/com/mateus/pokedex/microservice/service/PokemonService.kt
+++ src/main/kotlin/com/mateus/pokedex/microservice/service/PokemonService.kt
@@ -5,6 +5,7 @@ import com.mateus.pokedex.microservice.helper.pokemonQuery.Companion.getUrl
 import com.mateus.pokedex.microservice.helper.pokemonQuery.Companion.withLimit
 import com.mateus.pokedex.microservice.model.PokemonApiResponse
 import com.mateus.pokedex.microservice.model.Pokemons
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
@@ -17,6 +18,12 @@ class PokemonService {
 
     private lateinit var pokemonsList: List<Pokemons>
 
+    private var sortByAlphabetical = "alphabetical"
+    private var sortByLength = "length"
+    private val allowedSort: List<String> = listOf(sortByAlphabetical, sortByLength)
+    private var sortType = sortByAlphabetical // alphabetical as default
+
+
     fun getPokemons(url: String): PokemonApiResponse {
         val data = this.restTemplate.getForObject<PokemonApiResponse>(url)
         this.pokemonsList = data.results
@@ -26,18 +33,16 @@ class PokemonService {
     fun getPokemonsByName(name: String?, sort: String?): List<Pokemons> {
         // TODO: Make cache logic
         if (!::pokemonsList.isInitialized || pokemonsList.isEmpty()) {
+            // Using 1304 as limit to bring all pokemons
             val url = pokemonQuery.createQuery().withLimit(1304).getUrl()
             this.getPokemons(url)
         }
-
-        //TODO: Filter pokemon list by name an sort it
-        if (name != null) {
-
+        if (!sort.isNullOrEmpty() && sort in this.allowedSort) {
+            this.sortType = sort
         }
 
         this.pokemonsList = this.mergeSortPokemonList(this.pokemonsList)
-        this.pokemonsList = this.filterPokemonList(this.pokemonsList)
-
+        this.pokemonsList = this.filterPokemonList(this.pokemonsList, name)
 
         return this.pokemonsList
     }
@@ -61,7 +66,11 @@ class PokemonService {
         var j: Int = 0
 
         while (i < leftHalf.size && j < rightHalf.size) {
-            if (leftHalf[i].name!! <= rightHalf[j].name!!) {
+            var compare: Boolean = leftHalf[i].name!! <= rightHalf[j].name!!
+            if (sortType == sortByLength) {
+                compare = leftHalf[i].name!!.length <= rightHalf[j].name!!.length
+            }
+            if (compare) {
                 results.add(leftHalf[i])
                 i++
             } else {
@@ -83,9 +92,14 @@ class PokemonService {
         return results
     }
 
-    fun filterPokemonList(pokeList: List<Pokemons>): List<Pokemons> {
+    fun filterPokemonList(pokeList: List<Pokemons>, query: String?): List<Pokemons> {
+        if (query.isNullOrEmpty()) {
+            return pokeList
+        }
 
-        return pokeList
+        return pokeList.filter { pokemon ->
+            pokemon.name?.contains(query, ignoreCase = true) == true
+        }
     }
 
 }
